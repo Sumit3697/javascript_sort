fd29522e8a6338630c9c068ab15264ed9e8a4989
Sumit3697 <105008642+Sumit3697@users.noreply.github.com>
2024-01-04 15:33:32 +0530

    Update sortingtask.js

--- sortingtask.js
+++ sortingtask.js
@@ -6,7 +6,7 @@ function bubbleSort(arr) {
   for (var i = 0; i < len - 1; i++) {
     for (var j = 0; j < len - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
-        // Swap elements if they are in the wrong order
+       
         var temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
